f1ae22f9a7d460119613df0d26e4976f0d59200b
Suganth <suganthchandran07@gmail.com>
2024-07-27 18:27:46 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+import songRouter from './src/routes/SongRouter.js';
+import connectDB from './src/config/configDB.js';
+import connectCloudinary from './src/config/Cloudinary.js';
+import albumRouter from './src/routes/AlbumRouter.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send("API ");
-})
+app.use('/api/song',songRouter);
+app.use('/api/album',albumRouter);
 
 app.listen(port,()=>{
     console.log(`Server Running on ${port}`);
