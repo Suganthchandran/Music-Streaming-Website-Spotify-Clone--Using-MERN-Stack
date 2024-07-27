619649c257afdebfa37390b13d0baa85b9e772a4
Suganth <suganthchandran07@gmail.com>
2024-07-27 18:26:26 +0530

    Updated SongRouter

---
+++ server/src/routes/SongRouter.js
@@ -0,0 +1,11 @@
+import { addSong,listSong, removeSong } from "../controllers/SongController.js";
+import express from 'express';
+import upload from "../middleware/Multer.js";
+
+const songRouter = express.Router();
+
+songRouter.post('/add', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), addSong);
+songRouter.get('/list',listSong);
+songRouter.post('/delete',removeSong);
+
+export default songRouter;
