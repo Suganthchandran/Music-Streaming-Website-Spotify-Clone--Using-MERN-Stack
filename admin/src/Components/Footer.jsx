8a1d16927f9bd11899cc3be08138ae98ea73341b
Suganth <suganthchandran07@gmail.com>
2024-07-28 14:55:46 +0530

    Updated

---
+++ admin/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Footer.css';
+import { assets } from '../assets/assets';
+
+const Footer = () => {
+    return (
+        <footer className="admin-footer">
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ admin/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1 className='navbar-heading'>Admin Panel</h1>
+    </div>
+  )
+}
+
+export default Navbar
