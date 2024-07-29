aa5d94299f2104f760b26077295ca0270fcfa775
Suganth <suganthchandran07@gmail.com>
2024-07-29 15:22:12 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar } from '../components/Navbar'
 import '../styles/Home.css'
-import {albumsData} from '../assets/frontend-assets/assets'
-import {songsData} from '../assets/frontend-assets/assets'
 import { Albums } from '../components/Albums'
 import { Songs } from '../components/Songs'
+// import { songsData,albumsData } from '../assets/frontend-assets/assets'
+import { PlayerContext } from './PlayerContext'
 
 export const Home = () => {
+
+  const {songsData,albumsData} = useContext(PlayerContext);
+
   return (
     <>
-        <Navbar/>
-        <div className='home-main'>
+      <Navbar />
+      <div className='home-main'>
         <h1 className='home-album-heading'>Featured Charts</h1>
-          <div className='albumss'>
-            {albumsData.map((item,index)=>(<Albums key={index} image={item.image} name={item.name} desc={item.desc} id={item.id} />))}
-          </div>
-        <h1 className='home-song-heading'>Today's Biggest Hits</h1>
-          <div className='songss'>
-            {songsData.map((item,index)=>(<Songs key={index} image={item.image} name={item.name} desc={item.desc} id={item.id} />))}
-          </div>
+        <div className='albumss'>
+          {albumsData.map((item, index) => (<Albums key={index} image={item.image} name={item.name} desc={item.desc} id={item._id} />))}
         </div>
+        <h1 className='home-song-heading'>Today's Biggest Hits</h1>
+        <div className='songss'>
+          {songsData.map((item, index) => (<Songs key={index} image={item.image} name={item.name} desc={item.desc} id={item._id} />))}
+        </div>
+      </div>
     </>
   )
 }
