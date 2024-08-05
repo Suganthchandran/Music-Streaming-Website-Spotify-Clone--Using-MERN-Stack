b27add904b03cd45fffecb428c55fea8648c3ddf
Suganth <suganthchandran07@gmail.com>
2024-08-05 23:58:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ import songRouter from './src/routes/SongRouter.js';
 import connectDB from './src/config/configDB.js';
 import connectCloudinary from './src/config/Cloudinary.js';
 import albumRouter from './src/routes/AlbumRouter.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,15 +12,10 @@ connectDB();
 connectCloudinary();
 
 app.use(express.json());
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(cors({
+    origin: 'https://music-streaming-website-spotify-clone.onrender.com', // Your frontend URL without trailing slash
+  }));
+  
 
 app.use('/api/song',songRouter);
 app.use('/api/album',albumRouter);
